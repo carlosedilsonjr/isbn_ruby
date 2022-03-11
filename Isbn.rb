3ed5f7d63842a716a38183e0c5689a2f194490a4
Carlos Edilson Junior <carlosedilsonabj@gmail.com>
2022-03-11 12:26:33 -0300

    validate a isbn number

--- Isbn.rb
+++ Isbn.rb
@@ -1,5 +1,32 @@
 class Isbn
-    def test
-        puts 'classe isbn'
+    def initialize(isbnNumber)
+        if(!isbnNumber)
+            puts 'Expected a number.'
+        elsif(isbnNumber.to_s.size>13)
+            puts 'Number have more than 12/13 digits.'
+        elsif(isbnNumber.to_s.size<12)
+            puts "Number have less than 12/13 digits."
+        else
+            if(isbnNumber.to_s.size==12)
+                @isbnNumber=isbnNumber
+            else
+                @isbnNumber=isbnNumber.to_s.chop.to_i
+                @digitValidator=isbnNumber.to_s[-1].to_i
+            end
+        end
+    end
+
+    def validate
+        sum =0
+        array= @isbnNumber.to_s.chars
+        array.each_index{|i| 
+            if(i%2==0)
+                sum += array[i].to_i * 1
+            else
+                sum += array[i].to_i * 3
+            end}
+        result = 10 - sum%10
+        numberValidated = @isbnNumber.to_s.concat(result.to_s).to_i
+        puts "O número recbido foi #{@isbnNumber} e sua validação ficou como #{numberValidated}"
     end
 end
