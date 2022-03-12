336d67e77c046f525cd936727609db716f29a11b
Carlos Edilson Junior <carlosedilsonabj@gmail.com>
2022-03-11 21:51:02 -0300

    improving code

--- Isbn.rb
+++ Isbn.rb
@@ -1,55 +1,47 @@
 class Isbn
-    def initialize(isbnNumber)
-        if(!(Integer(isbnNumber) rescue false))
-            @result = 'Invalid input, give a number!'
-        elsif(isbnNumber.to_s.size>13)
-            @result = 'Number have more than 12/13 digits!'
-        elsif(isbnNumber.to_s.size<12)
-            @result = "Number have less than 12/13 digits!"
-        else
-            if(isbnNumber.to_s.size==12)
-                @isbnNumber=isbnNumber
-                self.validate
-            else
-                @isbnNumber=isbnNumber.to_s.chop.to_i
-                @digitValidator=isbnNumber.to_s[-1].to_i
-                @completeNumber=isbnNumber
-                self.check
-            end
-        end
-    end
+  def initialize(isbn_number)
+    @integer_input = isbn_number.is_a? Integer
+    @isbn_number = isbn_number.to_s
+    raise TypeError, "Expected a number!" unless @isbn_number.match?(/^\d+$/m)
+    raise ArgumentError, "The number have more than 12/13 digits!" if @isbn_number.size > 13
+    raise ArgumentError, "The number have less than 12/13 digits!" if @isbn_number.size < 12
 
-    def validate
-        sum =0
-        array= @isbnNumber.to_s.chars
-        array.each_index{|i| 
-            if(i%2==0)
-                sum += array[i].to_i * 1
-            else
-                sum += array[i].to_i * 3
-            end}
-        result = 10 - sum%10
-        result = 0 if result==10
-        numberValidated = @isbnNumber.to_s.concat(result.to_s).to_i
-        @result = numberValidated
+    if(@isbn_number.size==12)
+      self.validate
+    else
+      @complete_number = @isbn_number
+      @digit_validator = @isbn_number[-1].to_i
+      @isbn_number = @isbn_number.chop
+      self.check
     end
+  end
 
-    def check
-        sum =0
-        array= @isbnNumber.to_s.chars
-        array.each_index{|i| 
-            if(i%2==0)
-                sum += array[i].to_i * 1
-            else
-                sum += array[i].to_i * 3
-            end}
-        result = 10 - sum%10
-        result = 0 if result==10
-        valid = result.eql?(@digitValidator)
-        @result = valid
+  def validate
+    sum = 0
+    array = @isbn_number.chars
+    array.each_index do |i| 
+      sum += array[i].to_i * 1 if i % 2 == 0
+      sum += array[i].to_i * 3 if i % 2 == 1
     end
+    result = 10 - sum % 10
+    result = 0 if result == 10
+    number_validated = @isbn_number.concat(result.to_s)
+    @integer_input ? @result = number_validated.to_i : @result = number_validated
+  end
 
-    def result
-        return @result
+  def check
+    sum = 0
+    array = @isbn_number.chars
+    array.each_index do |i| 
+      sum += array[i].to_i * 1 if i % 2 == 0
+      sum += array[i].to_i * 3 if i % 2 == 1
     end
+    result = 10 - sum % 10
+    result = 0 if result == 10
+    @result = result.eql?(@digit_validator)
+  end
+
+  def result
+    @result
+  end
 end
--- app.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'Isbn'
-
-#isbn = Isbn.new(gets.chomp)
-isbn = Isbn.new(9780143007234)
-isbn2 = Isbn.new(9780143007236)
-puts isbn.result
-puts isbn2.result
